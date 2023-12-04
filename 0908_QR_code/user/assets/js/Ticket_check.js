8935789ed4faca825a794c7e0e779b03ac82cf32
hj201722 <torl123491@gmail.com>
2023-12-04 13:17:48 +0900

    dd
    d

--- 0908_QR_code/user/assets/js/Ticket_check.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('agree-button').addEventListener('click', function() {
-    const ticketNumber = document.getElementById('ticket-number').value;
-    localStorage.setItem('ticketNumber', ticketNumber);
-    // 同意が完了した後の処理
-});
